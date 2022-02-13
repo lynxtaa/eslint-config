a7dbc7238f016a4e321d116bfdafd11324ff4d48
Alex Kondratyuk <lynxtaa@gmail.com>
2022-02-13 14:29:45 +0300

    fix: remove buggy typescript resolver

--- index.js
+++ index.js
@@ -13,19 +13,6 @@ module.exports = {
 		sourceType: 'module',
 		ecmaVersion: 'latest',
 	},
-	settings: {
-		'import/parsers': {
-			[require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
-		},
-		'import/resolver': {
-			[require.resolve('eslint-import-resolver-node')]: {
-				extensions: ['.js', '.jsx', '.ts', '.tsx'],
-			},
-			[require.resolve('eslint-import-resolver-typescript')]: {
-				alwaysTryTypes: true,
-			},
-		},
-	},
 	env: {
 		es6: true,
 		jest: true,
