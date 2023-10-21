71c061f5ce5f8774c2f85f65b26117fd65b82d0a
Alex Kondratiuk <lynxtaa@gmail.com>
2023-10-22 00:57:34 +0200

    fix: turn off no-explicit-any

--- requires-typechecking.js
+++ requires-typechecking.js
@@ -4,6 +4,7 @@ module.exports = {
 			files: ['*.ts?(x)'],
 			extends: ['plugin:@typescript-eslint/recommended-requiring-type-checking'],
 			rules: {
+				'@typescript-eslint/no-explicit-any': 'off',
 				'@typescript-eslint/no-unsafe-assignment': 'off',
 				'@typescript-eslint/no-unsafe-call': 'off',
 				'@typescript-eslint/no-unsafe-member-access': 'off',
